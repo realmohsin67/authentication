892373c81d35841390977f60d04447ccdbfd67d3
realmohsin67 <realmohsin67@gmail.com>
2025-05-09 22:52:22 -0400

    Initial commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Users from "@/components/users";
+import Posts from "@/components/posts";
+
+export default function Page() {
+  return (
+    <div>
+      <Users />
+      <Posts />
+    </div>
+  );
+}
